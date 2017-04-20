1f8a54314e1709a9f44cbb1c26fbf4daa1ff986d
Loc Tran <tomtran43@gmail.com>
2017-04-20 22:39:40 +0700

    sua loi nqueen

--- PDFViewController.swift
+++ PDFViewController.swift
@@ -12,6 +12,20 @@ import GoogleMobileAds
 class PDFViewController: UIViewController, GADBannerViewDelegate {
     
     var bannerView = GADBannerView()
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        AppUtility.lockOrientation(.portrait)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppUtility.lockOrientation(.all)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
